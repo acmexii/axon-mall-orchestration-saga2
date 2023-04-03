a5b2ffc77ecc806d68dfb5529cd5a1af485bf8d9
acmexii <apexacme@gmail.com>
2023-04-03 05:50:19 +0000

    u

--- common-api/src/main/java/axon/mallorchestrationsaga/command/CancelDeliveryCommand.java
+++ common-api/src/main/java/axon/mallorchestrationsaga/command/CancelDeliveryCommand.java
@@ -1,6 +1,5 @@
 package axon.mallorchestrationsaga.command;
 
-import java.util.List;
 import lombok.Data;
 import lombok.ToString;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
@@ -10,7 +9,5 @@ import org.axonframework.modelling.command.TargetAggregateIdentifier;
 public class CancelDeliveryCommand {
 
     @TargetAggregateIdentifier
-    private String deliveryId;
-
     private String deliveryId;
 }
--- common-api/src/main/java/axon/mallorchestrationsaga/command/OrderCancelCommand.java
+++ common-api/src/main/java/axon/mallorchestrationsaga/command/OrderCancelCommand.java
@@ -1,6 +1,5 @@
 package axon.mallorchestrationsaga.command;
 
-import java.util.List;
 import lombok.Data;
 import lombok.ToString;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
@@ -10,7 +9,5 @@ import org.axonframework.modelling.command.TargetAggregateIdentifier;
 public class OrderCancelCommand {
 
     @TargetAggregateIdentifier
-    private String orderId;
-
     private String orderId;
 }
